325ef29ad1cfc7f67f88b76db8231ac0a32d359c
Shane Upchurch <shaneupchurch@gmail.com>
2021-02-04 16:41:52 -0600

    some styling on request form

--- src/components/request/Request.jsx
+++ src/components/request/Request.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Request.css';
 
 const Request = ({ onSubmit, handleChange }) => (
-  <form onSubmit={onSubmit}>
-    <input type="url" onChange={handleChange}/>
-    <div>
-      <input type="radio" value="GET" id="get" name="method"/>
-      <label for="get">GET</label>
-      <input type="radio" value="POST" id="post" name="method"/>
-      <label for="post">POST</label>
-      <input type="radio" value="PUT" id="put" name="method"/>
-      <label for="put">PUT</label>
-      <input type="radio" value="PATCH" id="patch" name="method"/>
-      <label for="patch">PATCH</label>
-      <input type="radio" value="DELETE" id="delete" name="method"/>
-      <label for="delete">DELETE</label>
-    </div>
-    <input type="submit" value="Let's Go!"/>
-  </form>
+  <>
+    <form onSubmit={onSubmit} className={styles.Request}>
+      <input type="url" placeholder="https://" onChange={handleChange}/>
+      <div>
+        <input type="radio" value="GET" id="get" name="method"/>
+        <label htmlFor="get">GET</label>
+        <input type="radio" value="POST" id="post" name="method"/>
+        <label htmlFor="post">POST</label>
+        <input type="radio" value="PUT" id="put" name="method"/>
+        <label htmlFor="put">PUT</label>
+        <input type="radio" value="PATCH" id="patch" name="method"/>
+        <label htmlFor="patch">PATCH</label>
+        <input type="radio" value="DELETE" id="delete" name="method"/>
+        <label htmlFor="delete">DELETE</label>
+      </div>
+      <input type="submit" value="Let's Go!"/>
+    </form>
+    <textarea cols="40" rows="10"/>
+  </>
 );
 
 Request.propTypes = {
